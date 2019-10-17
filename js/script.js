801923f538bda94786dc96c5e737d7b9c1337682
TANS123 <t.a.secrease@gmail.com>
2019-10-17 01:28:57 +0000

    I appended the sliced words to the page and i accomplished turning a single word to piglatin

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $( "document" ).ready(function() {
+$("#button").click(function(){
+ var latinInput = $(".input").val();
+ var str = latinInput;
+ var n = str.search(/[aeiouAEIOU]/);
+ var end = str.slice(n);
+ var beg = str.slice(0, n);
+ $("p").append(end + beg + "ay");
 
-
-
+});
 });
