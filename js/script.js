51794109e8ed3bd6fe2df3ff861e547068215860
TANS123 <t.a.secrease@gmail.com>
2019-10-24 01:24:50 +0000

    I got a sentence to get translated into pi latin by using forEach loops and if command if there is no vowel

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ $( "document" ).ready(function() {
 $("#button").click(function(){
  var latinInput = $(".input").val();
  var str = latinInput;
- var n = str.search(/[aeiouAEIOU]/);
- var end = str.slice(n);
- var beg = str.slice(0, n);
- $("p").append(end + beg + "ay");
+ var results = ''
+ var wrds = str.split(' ')
+ wrds.forEach(function(letter) {
+     var n = letter.search(/[aeiouAEIOU]/);
+     if (n == -1) {
+         n = 0
+     }
+    var end = letter.slice(n);
+    var beg = letter.slice(0, n);
+    var pig = end + beg + "ay"
+    results = results + pig + ' ';
+ });
+ $("p").text(results);
+
+
+
 
 });
 });
